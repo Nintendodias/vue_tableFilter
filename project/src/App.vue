65ec03f8d8723926cf7ac0d600dac46e51e61219
Anastasiya <40817643+Nintendodias@users.noreply.github.com>
2021-01-28 23:28:03 +0300

    finish all tasks

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<div class="wrapper">
+			<tableItem />
+			<formItem />
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+	import tableItem from './components/tableItem.vue';
+  import formItem from './components/formItem.vue';
+  
+	import 'bootstrap/dist/css/bootstrap.css';
+	import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+	export default {
+		name: 'App',
+		components: {
+			tableItem,
+			formItem,
+		},
+	};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	.wrapper {
+		font-family: Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding-left: 20px;
+    padding-right: 20px;
+
+    section {
+      width: 100%;
+    }
+
+    button {
+      border-radius: 20px;
+      padding: .3em 1em;
+    }
+    p {
+      margin: 0;
+    }
+	}
 </style>
---
+++ project/src/components/formItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="form_container">
+		
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'formItem',
+	};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	.form_container {
+		width: 100%;
+	}
+</style>
